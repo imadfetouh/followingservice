6b41a846d5f86a73e4363f0f4f3dce8cbc43e337
imad <imad.elfetouh@hotmail.com>
2021-05-19 16:18:49 +0200

    add users

--- followingservice/src/main/java/com/imadelfetouh/followingservice/dal/queryexecuter/SetupDatabase.java
+++ followingservice/src/main/java/com/imadelfetouh/followingservice/dal/queryexecuter/SetupDatabase.java
@@ -1,12 +1,22 @@
 package com.imadelfetouh.followingservice.dal.queryexecuter;
 
 import com.imadelfetouh.followingservice.dal.configuration.QueryExecuter;
+import com.imadelfetouh.followingservice.dal.ormmodel.User;
 import com.imadelfetouh.followingservice.model.response.ResponseModel;
 import org.hibernate.Session;
 
 public class SetupDatabase implements QueryExecuter<Void> {
     @Override
     public ResponseModel<Void> executeQuery(Session session) {
-        return new ResponseModel<>();
+        ResponseModel<Void> responseModel = new ResponseModel<>();
+        User user1 = new User("u123", "imad", "imad.jpg");
+        User user2 = new User("u1234", "peter", "peter.jpg");
+
+        session.persist(user1);
+        session.persist(user2);
+
+        session.getTransaction().commit();
+
+        return responseModel;
     }
 }
