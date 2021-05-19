cf918801b5ea08d98523d9afe40940bc511d3480
imad <imad.elfetouh@hotmail.com>
2021-05-19 19:55:03 +0200

    system

--- followingservice/src/test/java/com/imadelfetouh/followingservice/FollowingRabbitTest.java
+++ followingservice/src/test/java/com/imadelfetouh/followingservice/FollowingRabbitTest.java
@@ -113,6 +113,7 @@ public class FollowingRabbitTest {
         Thread.sleep(2000);
 
         JsonObject jsonObject = gson.fromJson(responseEntity.getBody(), JsonObject.class);
+        System.out.println(jsonObject);
         Long followers = jsonObject.get("followers").getAsLong();
 
         Assertions.assertEquals(1, followers);
